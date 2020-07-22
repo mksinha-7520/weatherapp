895b2200c463c1716e77211f136920bb2e61f627
Manish Sinha <mk.ssit@gmail.com>
2020-07-22 08:03:17 -0700

    changes before heroku

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const forecast = require('./utils/forecast')
 //console.log(path.join(__dirname, '../public'))
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define path for config
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -23,8 +24,8 @@ app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partials)
 
-app.listen(3000, () => {
-    console.log(chalk.inverse.green('server has been started on port 30000!!'))
+app.listen(port, () => {
+    console.log(chalk.inverse.green('server has been started on port  '+port))
 })
 
 app.get('', (req, res) => {
